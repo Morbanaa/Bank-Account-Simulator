82625204eb7d20637b544f71d197c6c89892aa0e
Morbanaa <theodorerodd@gmail.com>
2026-05-15 18:19:23 -0500

    Set up bank class constructor and methods

--- classes.py
+++ classes.py
@@ -1,1 +1,12 @@
-ECHO is on.
+class Bank():
+    def __init__(self,balance):
+        self.balance = balance
+
+    def deposit(self):
+        pass
+
+    def withdraw(self):
+        pass
+
+    def check_balance(self):
+        pass

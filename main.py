97502663f2dce95eabc71d3bfa2a94eb31610290
Morbanaa <theodorerodd@gmail.com>
2026-05-15 18:46:17 -0500

    Set up main loop which prints the menu and stores the users choice. After collecting choice it determins next steps with match case. Created clear screen function to improve user UI

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
+import platform
+import os
 from classes import Bank
 
 def main():
-    pass
+    bank = Bank(1000)
+    while True:
+        clear_screen()
+        print("-------------------------------")
+        print("               Bank")
+        print("-------------------------------\n")
+
+        print("Would you like to:")
+        print("(A) Check Balance:")
+        print("(B) Withdraw Funds:")
+        print("(C) Deposit Funds:")
+        print("(D) Exit Program:")
+        choice = input("Choose: ").upper()
+
+        match choice:
+            case "A":
+                pass
+            case "B":
+                pass
+            case "C":
+                pass
+            case "D":
+                break
+            case _:
+                continue
+
+def clear_screen():
+    if platform.system() == "Windows":
+        os.system("cls")
+    else:
+        os.system("clear")
 
 if __name__ == "__main__":
     main()
+
+    clear_screen()
+    print("\nHave a great day\n")

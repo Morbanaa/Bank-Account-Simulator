aa89ce8e83c25fdf14c7a476a91f270a9d05548b
Morbanaa <theodorerodd@gmail.com>
2026-05-16 16:30:21 -0500

    Completed deposit method. Takes user input while protecting against non number values and numbers less than or = to 0. Updates the balance if a value is valid

--- classes.py
+++ classes.py
@@ -3,7 +3,19 @@ class Bank():
         self.balance = balance
 
     def deposit(self):
-        pass
+        while True:
+            try:
+                amount = float(input("Enter amount to deposit: "))
+
+                if amount <= 0:
+                    print("The value must be greater than zero!")
+                    continue
+                else:
+                    self.balance += amount
+
+                break
+            except ValueError:
+                print("Value Error! Must be a number!")
 
     def withdraw(self):
         while True:
